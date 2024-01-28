e464a6cdd1365dfb2e1c6cbbef29868188c78733
tireks <33616425+tireks@users.noreply.github.com>
2024-01-28 15:26:43 +0700

    ui for users screen

--- ComposeBased/app/src/main/java/com/tirexmurina/composerandomusr/screen/UserDetailsScreen.kt
+++ ComposeBased/app/src/main/java/com/tirexmurina/composerandomusr/screen/UserDetailsScreen.kt
@@ -1,5 +1,6 @@
 package com.tirexmurina.composerandomusr.screen
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,6 +11,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ComposeCompilerApi
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -19,10 +23,52 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import coil.compose.rememberAsyncImagePainter
 import com.tirexmurina.composerandomusr.R
+import com.tirexmurina.composerandomusr.domain.entity.User
+import com.tirexmurina.composerandomusr.presentation.UserViewModel
+import com.tirexmurina.composerandomusr.presentation.UserViewState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import okhttp3.internal.toHexString
 
 @Composable
-fun UserDetailsScreen() {
+fun UserDetailsScreen(
+    viewModel: UserViewModel = hiltViewModel(),
+    id : String?
+) {
+
+    Log.d("BL", "RECOMPOSED-USER_SCREEN")
+    Log.d("BL","viewModel ${viewModel.hashCode().toHexString()}")
+    DisposableEffect(key1 = Unit){
+        if(!id.isNullOrBlank()){
+            viewModel.getUserById(id)
+        }
+        onDispose {}
+    }
+
+    val viewState by remember { viewModel.state }
+    when(val bufState = viewState){
+        is UserViewState.Initial ->{
+
+        }
+
+        is UserViewState.Content -> {
+            UserDetailsScreenContent(user = bufState.data)
+        }
+        is UserViewState.Error -> {
+            Text(text = "Error ${bufState.errorMsg}")
+        }
+        is UserViewState.Loading -> {
+            Text(text = "Loading")
+            //todo тут чета сделать
+        }
+    }
+}
+
+@Composable
+fun UserDetailsScreenContent(user: User){
     Column {
         Box(modifier = Modifier
             .height(230.dp)
@@ -30,14 +76,17 @@ fun UserDetailsScreen() {
             .padding(top = 16.dp, bottom = 4.dp)
         ){
             Image(
-                painterResource(id = R.drawable.person0placeholder),
+                painter = rememberAsyncImagePainter(
+                    user.picture.large
+                ),
                 contentDescription = null,
                 contentScale = ContentScale.FillHeight,
                 modifier = Modifier.fillMaxSize()
             )
         }
         Text (
-            text = "Ms",
+            //text = user.name.title,
+            text = user.id.substringAfter("|||"),
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
                 .padding(bottom = 4.dp, top = 4.dp),
@@ -46,7 +95,8 @@ fun UserDetailsScreen() {
             fontStyle = FontStyle.Italic
         )
         Text (
-            text = "Rebecca Composinski",
+            //text = user.name.first + " " + user.name.last,
+            text = user.id.substringBefore("|||"),
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
                 .padding(bottom = 4.dp, top = 4.dp),
@@ -54,7 +104,7 @@ fun UserDetailsScreen() {
             fontWeight = FontWeight.Bold
         )
         Text (
-            text = "Female",
+            text = user.gender,
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
                 .padding(bottom = 4.dp, top = 4.dp),
@@ -71,7 +121,7 @@ fun UserDetailsScreen() {
             fontStyle = FontStyle.Italic
         )
         Text (
-            text = "testmail@mail.com",
+            text = user.email,
             modifier = Modifier
                 .padding(start = 16.dp, bottom = 4.dp, top = 4.dp),
             fontSize = 20.sp,
@@ -86,7 +136,7 @@ fun UserDetailsScreen() {
             fontStyle = FontStyle.Italic
         )
         Text (
-            text = "0176-2908287",
+            text = user.cell,
             modifier = Modifier
                 .padding(start = 16.dp, bottom = 4.dp, top = 4.dp),
             fontSize = 20.sp,
@@ -101,7 +151,7 @@ fun UserDetailsScreen() {
             fontStyle = FontStyle.Italic
         )
         Text (
-            text = "GB",
+            text = user.nat,
             modifier = Modifier
                 .padding(start = 16.dp, bottom = 4.dp, top = 4.dp),
             fontSize = 20.sp,
@@ -116,48 +166,41 @@ fun UserDetailsScreen() {
             fontStyle = FontStyle.Italic
         )
         Text (
-            text = "Finland",
+            text = user.location.country,
             modifier = Modifier
                 .padding(start = 16.dp, bottom = 4.dp, top = 4.dp),
             fontSize = 20.sp,
             fontWeight = FontWeight.SemiBold
         )
         Text (
-            text = "Northern Savonia",
+            text = user.location.state,
             modifier = Modifier
                 .padding(start = 16.dp, bottom = 4.dp, top = 4.dp),
             fontSize = 20.sp,
             fontWeight = FontWeight.SemiBold
         )
         Text (
-            text = "Kumlinge",
+            text = user.location.city,
             modifier = Modifier
                 .padding(start = 16.dp, bottom = 4.dp, top = 4.dp),
             fontSize = 20.sp,
             fontWeight = FontWeight.SemiBold
         )
         Text (
-            text = "Suvantokatu" + " 1440",
+            text = user.location.street.name,
             modifier = Modifier
                 .padding(start = 16.dp, bottom = 4.dp, top = 4.dp),
             fontSize = 20.sp,
             fontWeight = FontWeight.SemiBold
         )
         Text (
-            text = "Coordinates:",
+            text = user.location.street.number,
             modifier = Modifier
                 .padding(start = 16.dp, bottom = 4.dp, top = 4.dp),
             fontSize = 14.sp,
             fontWeight = FontWeight.Normal,
             fontStyle = FontStyle.Italic
         )
-        /*Text (
-            text = "-25.1328" + "    -51.2658",
-            modifier = Modifier
-                .padding(start = 16.dp, bottom = 4.dp, top = 4.dp),
-            fontSize = 20.sp,
-            fontWeight = FontWeight.SemiBold
-        )*/
         Row(){
             Column(modifier = Modifier
                 .padding(start = 16.dp)
@@ -171,7 +214,7 @@ fun UserDetailsScreen() {
                     fontStyle = FontStyle.Italic
                 )
                 Text (
-                    text = "-25.1328",
+                    text = user.location.coordinates.latitude,
                     modifier = Modifier
                         .padding(bottom = 4.dp, top = 4.dp),
                     fontSize = 20.sp,
@@ -190,7 +233,7 @@ fun UserDetailsScreen() {
                     fontStyle = FontStyle.Italic
                 )
                 Text (
-                    text = "-51.2658",
+                    text = user.location.coordinates.longitude,
                     modifier = Modifier
                         .padding(bottom = 4.dp, top = 4.dp),
                     fontSize = 20.sp,
@@ -204,5 +247,5 @@ fun UserDetailsScreen() {
 @Preview(showBackground = true)
 @Composable
 fun PreviewScreen(){
-    UserDetailsScreen()
+    //UserDetailsScreen()
 }
